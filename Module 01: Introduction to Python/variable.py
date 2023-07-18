a327b64e1b8f91c0d00106e5a344cc03e1b8387b
alisadiq2003 <alisadiq.personal2003@gmail.com>
2023-07-18 18:18:54 +0500

    first commit

---
+++ Module 01: Introduction to Python/variable.py
@@ -0,0 +1,10 @@
+# What is variable?
+# Variable is used to store data/value.
+
+# declare and intiailzie a variable 
+
+name = "Ehtisham Sadiq"
+print(name)
+print(type(name))
+name_other = name.replace("Ehtisham","Ali")
+print(name_other)
